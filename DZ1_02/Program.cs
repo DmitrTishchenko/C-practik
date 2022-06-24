a550a74b136ac073841149f0c04aca2968729460
Tishchenko_D <tishchenko.1993@list.ru>
2022-06-24 11:55:27 +0300

    Задача 4

---
+++ DZ1_02/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+           Console.WriteLine("Введите целое число A: ");
+           int a = Convert.ToInt32(Console.ReadLine());
+           
+           Console.WriteLine("Введите целое число B: ");
+           int b = Convert.ToInt32(Console.ReadLine());
+
+           Console.WriteLine("Введите целое число C: ");
+           int c = Convert.ToInt32(Console.ReadLine());
+
+           int max = Math.Max(Math.Max(a, b), c);
+
+        
+            Console.Write("max = ");
+            Console.WriteLine(max);
+            
+            
+
+        }
+    }
+}
