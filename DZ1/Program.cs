3376195a1f370be69ebb2e02e2977b80fea35f41
Tishchenko_D <tishchenko.1993@list.ru>
2022-06-24 11:20:19 +0300

    Задача 2

---
+++ DZ1/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //    Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+           
+           Console.WriteLine("Введите целое число A: ");
+           int numberA = Convert.ToInt32(Console.ReadLine());
+           
+           Console.WriteLine("Введите целое число B: ");
+           int numberB = Convert.ToInt32(Console.ReadLine());
+
+           
+        
+           if (numberA > numberB)
+            {
+                int max = numberA;
+                int min = numberB;
+                Console.Write("max = ");
+                Console.WriteLine(max);
+
+                Console.Write("min = ");
+                Console.WriteLine(min);
+
+            }
+            if (numberB > numberA)
+            {
+                int max = numberB;
+                int min = numberA;
+                Console.Write("max = ");
+                Console.WriteLine(max);
+
+                Console.Write("min = ");
+                Console.WriteLine(min);
+            }
+            if (numberA == numberB)
+            {
+                Console.Write("Числа равны");
+            }
+        
+
+        }
+    }
+}
