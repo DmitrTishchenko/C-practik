099a4d08f5376a867c86b6c76b36d8dc4fc121d6
Tishchenko_D <tishchenko.1993@list.ru>
2022-06-24 13:12:01 +0300

    Задача 6

---
+++ DZ1_03/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+           Console.WriteLine("Введите целое число: ");
+           int a = Convert.ToInt32(Console.ReadLine());
+
+           if ((a%2) == 0)
+           {
+               Console.WriteLine($"{ a } - число четное");
+           }
+           else
+           {
+               Console.WriteLine($"{ a } - число нечетное");
+           }
+           
+
+        }
+    }
+}
