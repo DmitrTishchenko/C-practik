eef6d6cf6680623d806a9bf5a4354386d7afd534
Tishchenko_D <tishchenko.1993@list.ru>
2022-06-26 10:59:47 +0300

    Доп задача в ДЗ

---
+++ DZ1_05/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+           Console.Clear();
+           Console.WriteLine("Введите целое число: ");
+           int n = Convert.ToInt32(Console.ReadLine());
+           
+           int firstDigit = (int)(n / Math.Pow(10, (int)Math.Log10(n)));// выводит первую цифру в числе
+           int lastDigit = (int)(n % 10); //выводит последнюю цифру в числе через остаток 
+           
+           
+           Console.Write(lastDigit);
+        }
+    }
+}
