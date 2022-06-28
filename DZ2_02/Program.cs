158ef988e06c892f9a1f48d7b190cee1ce5951c2
Tishchenko_D <tishchenko.1993@list.ru>
2022-06-28 16:31:06 +0300

    Задача 13

---
+++ DZ2_02/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Geekbrains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача 13: Напишите программу, которая выводит третью цифру 
+            //            заданного числа или сообщает, что третьей цифры нет.
+            Console.Clear();
+            Console.WriteLine("Введите число: ");
+            string? str = Console.ReadLine();
+
+            if (int.TryParse(str, out int val))
+            {
+                if (str.Length > 2)
+                {
+                    Console.WriteLine($"Третья цифра цисла {str} это {str[2]}");                         
+
+                }
+                else Console.WriteLine("Третьей цифры нет");
+            }
+            else Console.WriteLine("Неверный код");
+
+        }
+    }
+
+}
