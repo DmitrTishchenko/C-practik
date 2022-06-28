6dc13752409cc14ddc07f2609e4b5aecbed3f4a0
Tishchenko_D <tishchenko.1993@list.ru>
2022-06-28 11:22:47 +0300

    Задача 10

---
+++ DZ2_01/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача 10: Напишите программу, которая принимает на вход трёхзначное
+            //            число и на выходе показывает вторую цифру этого числа.
+           Console.Clear();
+           Console.WriteLine("Введите целое трехзначное число: ");
+           int n = Convert.ToInt32(Console.ReadLine());
+
+           int lastDigit = (int)(n % 100);
+
+           int firstDigit = (int)(lastDigit / Math.Pow(10, (int)Math.Log10(lastDigit)));
+           Console.Write(firstDigit);
+
+       
+        }    
+ 
+    }
+}
