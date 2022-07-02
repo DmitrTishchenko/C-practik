58b550d73138158cdb0c73539648feff18d2740c
Tishchenko_D <tishchenko.1993@list.ru>
2022-07-02 20:33:55 +0300

    Задача 23

---
+++ DZ3_03/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход
+//            число (N) и выдаёт таблицу кубов чисел от 1 до N.
+namespace Geekbrains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Clear();
+           Console.WriteLine("Введите число N: ");
+           int a = Convert.ToInt32(Console.ReadLine());
+           int [] cube = GetCube(a);
+           for (int i = 0; i < cube.Length; i++)
+           {
+               Console.Write(cube[i]+ ", ");
+           }
+            
+        }
+        static int [] GetCube (int n)
+        {
+            int[] cube = new int [n];
+            for (int i=1; i <= n; i++)
+            {
+                cube[i-1] = i*i*i;
+            }
+            return cube;
+        }
+
+    }
+}
