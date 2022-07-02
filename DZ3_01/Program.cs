e6cffcf30fecdd84cc1d343ab7da05720ca928f2
Tishchenko_D <tishchenko.1993@list.ru>
2022-07-02 16:05:39 +0300

    Задача 19

---
+++ DZ3_01/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход 
+//           пятизначное число и проверяет, является ли оно палиндромом.
+
+
+namespace Palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            while (true)
+            {
+                Console.Write("Введите число: ");
+                if (int.TryParse(Console.ReadLine(), out number))
+                    break;
+                else
+                    Console.WriteLine("Неверное число.");
+            }
+            int oldValue = number;
+            int newValue = 0;
+            while (number > 0)
+            {
+                int dig = number % 10;
+                newValue = newValue * 10 + dig;
+                number = number / 10;
+            }
+            if (newValue == oldValue)
+                Console.WriteLine("Число является палиндромом");
+            else
+                Console.WriteLine("Число не является палиндромом");
+            
+        }
+    }
+}
